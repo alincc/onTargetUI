9d3fb833644806ae9b9cc5c04b5aae46fd9c842a
Sanjeev Ghimire <gsanjeev7@gmail.com>
2015-09-10 14:11:39 -0400

    added configurations

--- config.js
+++ config.js
@@ -1,3 +1,7 @@
+/**
+ * NOTE:: For local testing either make use of default or local and don't commit that changes.
+ *
+ */
 module.exports = {
   default: {
     port: 3214,
@@ -6,34 +10,34 @@ module.exports = {
     nodeServer: 'http://demo.newoceaninfosys.com:3215'
   },
   local: {
-    port: 3214,
-    domain: 'http://demo.newoceaninfosys.com:3215/ontargetrs/services',
-    baseUrl: 'http://demo.newoceaninfosys.com:3214',
-    nodeServer: 'http://demo.newoceaninfosys.com:3215'
+    port: 9000,
+    domain: 'http://localhost:9001/ontargetrs/services',
+    baseUrl: 'http://localhost:8080',
+    nodeServer: 'http://localhost:9001'
   },
   integration: {
-    port: 3214,
-    domain: 'http://demo.newoceaninfosys.com:3215/ontargetrs/services',
-    baseUrl: 'http://demo.newoceaninfosys.com:3214',
-    nodeServer: 'http://demo.newoceaninfosys.com:3215'
+    port: 9000,
+    domain: 'int.app.ontargetcloud.com:9001/ontargetrs/services',
+    baseUrl: 'int.api.ontargetcloud.com:8080',
+    nodeServer: 'int.app.ontargetcloud.com:9001'
   },
   testing: {
-    port: 3214,
-    domain: 'http://demo.newoceaninfosys.com:3215/ontargetrs/services',
-    baseUrl: 'http://demo.newoceaninfosys.com:3214',
-    nodeServer: 'http://demo.newoceaninfosys.com:3215'
+    port: 0,
+    domain: 'http://hostname:port/ontargetrs/services',
+    baseUrl: 'http://hostname:port',
+    nodeServer: 'http://hostname:port'
   },
   staging: {
-    port: 3214,
-    domain: 'http://demo.newoceaninfosys.com:3215/ontargetrs/services',
-    baseUrl: 'http://demo.newoceaninfosys.com:3214',
-    nodeServer: 'http://demo.newoceaninfosys.com:3215'
+    port: 0,
+    domain: 'http://hostname:port/ontargetrs/services',
+    baseUrl: 'http://hostname:port',
+    nodeServer: 'http://hostname:port'
   },
   production: {
-    port: 3214,
-    domain: 'http://demo.newoceaninfosys.com:3215/ontargetrs/services',
-    baseUrl: 'http://demo.newoceaninfosys.com:3214',
-    nodeServer: 'http://demo.newoceaninfosys.com:3215'
+    port: 0,
+    domain: 'http://hostname:port/ontargetrs/services',
+    baseUrl: 'http://hostname:port',
+    nodeServer: 'http://hostname:port'
   },
   server: {
     PROXY_URL: 'http://app.ontargetcloud.com:8080/ontargetrs/services',
@@ -46,7 +50,22 @@ module.exports = {
     maxFileSize: 5000000 // in bytes
   },
   serverintegration: {
-    PROXY_URL: 'http://app.ontargetcloud.com:8080/ontargetrs/services',
+    PROXY_URL: 'http://int.api.ontargetcloud.com:8080/ontargetrs/services',
+    assetLocation: 'assets/', // empty is root
+    maxFileSize: 5000000 // in bytes
+  },
+  servertesting: {
+    PROXY_URL: 'http://hostname:port/ontargetrs/services',
+    assetLocation: 'assets/', // empty is root
+    maxFileSize: 5000000 // in bytes
+  },
+  serverstaging: {
+    PROXY_URL: 'http://hostname:port/ontargetrs/services',
+    assetLocation: 'assets/', // empty is root
+    maxFileSize: 5000000 // in bytes
+  },
+  serverproduction: {
+    PROXY_URL: 'http://hostname:port/ontargetrs/services',
     assetLocation: 'assets/', // empty is root
     maxFileSize: 5000000 // in bytes
   }
