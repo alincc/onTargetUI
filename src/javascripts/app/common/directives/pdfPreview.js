4e55c805519eb2246f79e6039916d6f69f85fd9f
daominhsangvn <sang.dao@newoceaninfosys.com>
2015-10-20 15:41:37 +0700

    fix pdf previewer

--- src/javascripts/app/common/directives/pdfPreview.js
+++ src/javascripts/app/common/directives/pdfPreview.js
@@ -1,21 +1,32 @@
-/**
- * Created by aashiskhanal on 10/19/15.
- */
 define(function(require) {
-    'use strict';
-    var angular = require('angular'),
-        config = require('app/config');
-    var module = angular.module('common.directives.pdfPreview', ['app.config']);
-    module.directive('pdf', function() {
-        return {
-            restrict: 'E',
-            link: function(scope, element, attrs) {
-                var url = scope.$eval(attrs.src);
-                element.replaceWith('<object width="100%" height="100%" type="application/pdf" data="' + url + '"></object>');
-            }
-        };
-    });
+  'use strict';
+  var angular = require('angular'),
+  config = require('app/config');
+  var module = angular.module('common.directives.pdfPreview', ['app.config']);
+  module.directive('pdf', function() {
+    return {
+      restrict: 'E',
+      //replace: true,
+      template:'<embed width="100%" height="100%" alt="pdf" pluginspage="http://www.adobe.com/products/acrobat/readstep2.html">',
+      scope: {
+        src: '@'
+      },
+      link: function(scope, element, attrs) {
+        //element.replaceWith('<object width="100%" height="100%" type="application/pdf" data="' + url + '"></object>');
+        var current = element.children();
+        console.log(current);
+        scope.$watch('src', function () {
+          var clone = element
+            .children()
+            .clone()
+            .attr('src', attrs.src);
+          current.replaceWith(clone);
+          current = clone;
+        });
+      }
+    };
+  });
 
-    return module;
+  return module;
 });
 
