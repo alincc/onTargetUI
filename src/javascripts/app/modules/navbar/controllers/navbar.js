06f30a81c8584c829997eded7b6fc54e66d94d0d
daominhsangvn <sang.dao@newoceaninfosys.com>
2015-12-07 08:45:36 +0700

    Happy JSHint

--- src/javascripts/app/modules/navbar/controllers/navbar.js
+++ src/javascripts/app/modules/navbar/controllers/navbar.js
@@ -5,6 +5,7 @@ define(function(require) {
     function($scope, appConstant, accountFactory, $state, $location, notifications, $rootScope, $modal, companyFactory, pushFactory, userNotificationsFactory, toaster, $q) {
       $scope.app = appConstant.app;
       $scope.viewNotifications = [];
+      var canceler;
 
       function bindInfo() {
         $scope.userInfo = {
@@ -20,6 +21,42 @@ define(function(require) {
         //$scope.newNotifications = _.where($scope.allNotifications, {"status": "NEW"});
         $scope.totalUnreadNotification = $rootScope.userNotifications.totalUnreadNotification;
         $scope.viewNotifications = $rootScope.userNotifications.notificationList;
+      }
+
+      function getAllNotifications() {
+        if(canceler) {
+          canceler.resolve();
+        }
+        canceler = $q.defer();
+        userNotificationsFactory.getAll({
+          "pageNumber": 1,
+          "perPageLimit": 5
+        }, canceler).then(function(resp) {
+          $rootScope.userNotifications = resp.data;
+          notifications.getNotificationSuccess();
+        });
+      }
+
+      function bindProjectEvent(projectId) {
+        pushFactory.bind('private-project-' + projectId + ':user-' + $rootScope.currentUserInfo.userId, function(data) {
+          console.log('Incoming data for ' + 'private-project-' + projectId + ':user-' + $rootScope.currentUserInfo.userId + ': ', data);
+          getAllNotifications();
+          toaster.pop('info', 'Info', data.message);
+        });
+      }
+
+      function bindChannel() {
+        if($rootScope.currentUserInfo.userId) {
+          pushFactory.bind('private-user-' + $rootScope.currentUserInfo.userId, function(data) {
+            console.log('Incoming data for ' + 'private-user-' + $rootScope.currentUserInfo.userId + ': ', data);
+            getAllNotifications();
+            toaster.pop('info', 'Info', data.message);
+          });
+        }
+
+        if($rootScope.currentProjectInfo.projectId) {
+          bindProjectEvent($rootScope.currentProjectInfo.projectId);
+        }
       }
 
       if($rootScope.currentUserInfo && $rootScope.currentUserInfo.contact) {
@@ -76,43 +113,6 @@ define(function(require) {
           bindProjectEvent(data.newProject.projectId);
         }
       }, 1000));
-
-      var canceler;
-      var getAllNotifications = function() {
-        if(canceler) {
-          canceler.resolve();
-        }
-        canceler = $q.defer();
-        userNotificationsFactory.getAll({
-          "pageNumber": 1,
-          "perPageLimit": 5
-        }, canceler).then(function(resp) {
-          $rootScope.userNotifications = resp.data;
-          notifications.getNotificationSuccess();
-        });
-      };
-
-      function bindProjectEvent(projectId) {
-        pushFactory.bind('private-project-' + projectId + ':user-' + $rootScope.currentUserInfo.userId, function(data) {
-          console.log('Incoming data for ' + 'private-project-' + projectId + ':user-' + $rootScope.currentUserInfo.userId + ': ', data);
-          getAllNotifications();
-          toaster.pop('info', 'Info', data.message);
-        });
-      }
-
-      function bindChannel() {
-        if($rootScope.currentUserInfo.userId) {
-          pushFactory.bind('private-user-' + $rootScope.currentUserInfo.userId, function(data) {
-            console.log('Incoming data for ' + 'private-user-' + $rootScope.currentUserInfo.userId + ': ', data);
-            getAllNotifications();
-            toaster.pop('info', 'Info', data.message);
-          });
-        }
-
-        if($rootScope.currentProjectInfo.projectId) {
-          bindProjectEvent($rootScope.currentProjectInfo.projectId);
-        }
-      }
 
       bindChannel();
 
